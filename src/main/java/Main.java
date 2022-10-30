55c8c1092afc5609e1d94514f78145e130fe7155
Sabina <sabinagamidova721@gmail.com>
2022-10-30 16:45:37 +0200

    delete unnecessary comments from main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Application.run();//TODO настроить логи, чтобы они записывались в файлы (properties)
+        System.out.println(); //TODO как исключить файлы из gitignore
     }
 }
