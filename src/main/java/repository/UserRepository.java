a678b731c7da451f03be1494c250a7b39f2b10c9
Sabina <sabinagamidova721@gmail.com>
2022-11-06 11:36:42 +0200

    Updated car service Added unit tests

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -26,7 +26,7 @@ public class UserRepository {
             statement.setString(1, user.getName());
             statement.setString(2, user.getSurname());
             statement.setString(3, user.getPatronymic());
-            statement.setDate(4, user.getDateOfBirth());
+            statement.setDate(4, new java.sql.Date(user.getDateOfBirth().getTime()));
             statement.setString(5, user.getEmail());
             statement.setString(6, user.getPassword());
             statement.setObject(7, user.getRoleId());
@@ -83,7 +83,7 @@ public class UserRepository {
             statement.setString(1, user.getName());
             statement.setString(2, user.getSurname());
             statement.setString(3, user.getPatronymic());
-            statement.setDate(4, user.getDateOfBirth());
+            statement.setDate(4, new java.sql.Date(user.getDateOfBirth().getTime()));
             statement.setString(5, user.getEmail());
             statement.setString(6, user.getPassword());
             statement.setObject(7, user.getRoleId());
@@ -95,11 +95,19 @@ public class UserRepository {
         }
     }
 
-    public void delete(UUID id) {
-        String INACTIVATE = "UPDATE \"user\" SET status=FALSE WHERE id=? AND status;";
-        try (PreparedStatement statement = connection.prepareStatement(INACTIVATE)) {
+    public boolean isExistById(UUID id) {
+        String IS_EXIST = "SELECT EXISTS (SELECT 1 FROM \"user\" WHERE id=?);";
+        try (PreparedStatement statement = connection.prepareStatement(IS_EXIST)) {
             statement.setObject(1, id);
-            statement.execute();
+            ResultSet resultSet = statement.executeQuery();
+
+            boolean isExist = Boolean.FALSE;
+            if (resultSet.next()) {
+                isExist = resultSet.getBoolean(1);
+            }
+
+            resultSet.close();
+            return isExist;
         } catch (SQLException exception) {
             log.error("Can not process statement", exception);
             throw new CarRentalException(exception.getMessage());
--- src/main/java/services/user/UserService.java
+++ src/main/java/services/user/UserService.java
@@ -1,31 +1,87 @@
 package services.user;
 
+import connection.Transactionable;
+import exception.CarRentalException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import models.people.User;
+import org.apache.commons.lang3.StringUtils;
+import repository.UserRepository;
 import services.CrudGenericInterface;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
-public class UserService implements CrudGenericInterface<User> {
+@Slf4j
+@AllArgsConstructor
+public class UserService implements CrudGenericInterface<User>, Transactionable {
+    private UserRepository userRepository;
 
     @Override
-    public User insert(User user){
-        return null;
-    }
-
-    @Override
-    public User getById(UUID id) {
-        return null;
+    public User insert(User user) {
+        log.info("Trying to insert new user");
+        validateUser(user);
+        User insertedUser = wrapIntoTransaction(() -> userRepository.insert(user));
+        log.info("User has been inserted successfully");
+        return insertedUser;
     }
 
     @Override
     public List<User> getAll() {
-        return null;
+        log.info("Trying to get all users");
+        return userRepository.getAll();
     }
 
     @Override
-    public User update(User user){return null;}
+    public User getById(UUID id) {
+        log.info("Trying to get user by id");
+        if (Objects.isNull(id)) {
+            log.error("Can not find user. User id must be not null");
+            throw new CarRentalException("User id must be NOT null");
+        }
+        return userRepository.getById(id);
+    }
 
     @Override
-    public boolean delete(UUID id){return true;}
+    public User update(User user) {
+        log.info("Trying to update user {}", user);
+        validateUser(user);
+        if (!userRepository.isExistById(user.getId())) {
+            log.warn("User with id {} not found", user.getId());
+            throw new CarRentalException("User with id %s not found", user.getId());
+        }
+        User updatedUser = wrapIntoTransaction(() -> userRepository.update(user));
+        log.info("User has been updated successfully");
+        return updatedUser;
+    }
+
+    @Override
+    public boolean delete(UUID id) {
+        if (Objects.isNull(id)) {
+            log.error("Can not find user. User id must be not null");
+            throw new CarRentalException("Can't remove the user, his id must be NOT null");
+        }
+        return wrapIntoTransaction(() -> {
+            User user = userRepository.getById(id);
+            user.setStatus(Boolean.FALSE);
+            userRepository.update(user);
+            return user.isStatus() != Boolean.TRUE;
+        });
+    }
+
+    private void validateUser(User user) {
+        if (Objects.isNull(user)) {
+            log.error("User is null");
+            throw new CarRentalException("User must be NOT null");
+        }
+        if (StringUtils.isBlank(user.getName()) ||
+                StringUtils.isBlank(user.getSurname()) ||
+                StringUtils.isBlank(user.getPatronymic()) ||
+                StringUtils.isBlank(user.getEmail()) ||
+                StringUtils.isBlank(user.getPassword())) {
+            log.error("User properties have invalid format {}", user);
+            throw new CarRentalException("User has invalid data");
+        }
+    }
 }
